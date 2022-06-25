360900ec665481371c93bbda4116ee0ad7b9a5c4
Muhammad Rafiq <muhammadrafiq017@gmail.com>
2022-06-26 03:50:22 +0500

    code update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,26 +4,21 @@ import { StaticImage } from "gatsby-plugin-image"
 function Footer() {
   return (
     <div className=" footer-d mx-64 px-12 bg-white  h-auto my-24">
-      <div className="flex footer-wrap align-middle">
+      <div className="flex footer-wrap justify-between align-middle">
         <span className=" ">
           <StaticImage src="../images/facebook.png" className="my-6" />
-          <StaticImage src="../images/Line.png" className="mx-4" />
         </span>
         <span className="h-auto">
           <StaticImage src="../images/google.png" className="my-5" />
-          <StaticImage src="../images/Line.png" className="mx-4" />
         </span>
         <span className="h-auto">
           <StaticImage src="../images/Linked.png" className="my-6" />
-          <StaticImage src="../images/Line.png" class="mx-4" />
         </span>
         <span className="h-auto">
           <StaticImage src="../images/Microsoft.png" className="my-6" />
-          <StaticImage src="../images/Line.png" class="mx-4" />
         </span>
         <span className="h-auto">
           <StaticImage src="../images/yelp.png" className="my-6" />
-          <StaticImage src="../images/Line.png" class="mx-4" />
         </span>
         <span className="h-auto">
           <StaticImage src="../images/NextHealth.png" className="my-5" />
